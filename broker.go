0b2a7125f26be249554794af6a7769734cf52baf
James Hunt <james@niftylogic.com>
2018-01-14 13:11:49 -0500

    Fix fmt-spec mismatch in error message

--- broker.go
+++ broker.go
@@ -80,7 +80,7 @@ func (b *Broker) Provision(instanceID string, details brokerapi.ProvisionDetails
 
 	l.Debug("checking if we are over out service and/or plan limits")
 	if plan.OverLimit(db) {
-		l.Error("service limit exceeded for %s/%s", plan)
+		l.Error("service limit exceeded for %s/%s", plan.Service.Name, plan.Name)
 		return spec, fmt.Errorf("service limit exceeded for %s/%s", plan.Service.Name, plan.Name)
 	}
 
