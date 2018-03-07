c699a5c76ecba0c8ba02dc918bb21695c5dd47b9
aanelli <atanelli17@gmail.com>
2018-03-07 17:37:35 -0500

    added some comments on what needs to be done this week

--- bosh_maintenance.go
+++ bosh_maintenance.go
@@ -4,6 +4,27 @@ package main
 //  that checks the BOSH director and marks service instances whose backing deployment have disappeared as such.
 //   Otherwise, we can get into a situation where there are no deployments on the director,
 //    but a service is "at the limit" w.r.t. number of service deployments in the Vault index.
+
+//clear out of vault all of the secrets that belong to deployments that don't exist anymore
+//make sure that decrements quota counter for vault( we either maintain a count for how many of each service we have,
+// or we track it on a per deployment instance.  probably located in one big key in vault)
+//check vault for a giant key called the index?
+
+// use this code from services.go to find and mark deployments that no longer exist on the bosh director
+//  for _, s := range db.Data {
+// 		if ss, ok := s.(map[string]interface{}); ok {
+// 			service, haveService := ss["service_id"]
+// 			plan, havePlan := ss["plan_id"]
+// 			if havePlan && haveService {
+// 				if v, ok := service.(string); ok && v == p.Service.ID {
+// 					existingService += 1
+// 					if v, ok := plan.(string); ok && v == p.ID {
+// 						existingPlan += 1
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
 func serviceWithNoDeploymentCheck() {
 	l := Logger.Wrap("*")
 	l.Info("checking for service instances with no backing deployment")
